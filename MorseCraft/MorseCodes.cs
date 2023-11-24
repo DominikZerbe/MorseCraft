2b0634a1780e3d6d958b3298a746c9d5bd76824c
Dominik Zerbe <dominik.zerbe@naviga.de>
2023-11-24 16:31:34 +0100

    Methode zum Ausgeben der Geräusche und Grafiken entwickelt.

--- MorseCraft/MorseCodes.cs
+++ MorseCraft/MorseCodes.cs
@@ -1,17 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MorseCraft
 {
-    internal class MorseCodes
+    /// <summary>
+    /// Hier werden alle bekannten Morsecodes beim Programmstart reingeladen.
+    /// Alle Übersetzungen greifen hierauf zu.
+    /// </summary>
+    public static class CodeList
+    {
+
+        private static List<MorseCodes> _codes = new List<MorseCodes>();
+        public static List<MorseCodes> Codes
+        {
+            get { return _codes; }
+            set { _codes = value; }
+        }
+
+        public static void Initialize()
+        {
+
+            List<MorseCodes> Uebersetzungstabelle = new List<MorseCodes>()
+            {
+                // Lateinische Buchstaben
+                new MorseCodes("A", ".-"),
+                new MorseCodes("B", "-..."),
+                new MorseCodes("C", "-.-."),
+                new MorseCodes("D", "-.."),
+                new MorseCodes("E", "."),
+                new MorseCodes("F", "..-."),
+                new MorseCodes("G", "--."),
+                new MorseCodes("H", "...."),
+                new MorseCodes("I", ".."),
+                new MorseCodes("J", ".---"),
+                new MorseCodes("K", "-.-"),
+                new MorseCodes("L", ".-.."),
+                new MorseCodes("M", "--"),
+                new MorseCodes("N", "-."),
+                new MorseCodes("O", "---"),
+                new MorseCodes("P", ".--."),
+                new MorseCodes("Q", "--.-"),
+                new MorseCodes("R", ".-."),
+                new MorseCodes("S", "..."),
+                new MorseCodes("T", "-"),
+                new MorseCodes("U", "..-"),
+                new MorseCodes("V", "...-"),
+                new MorseCodes("W", ".--"),
+                new MorseCodes("X", "-..-"),
+                new MorseCodes("Y", "-.--"),
+                new MorseCodes("Z", "--.."),
+
+                // Zahlen
+                new MorseCodes("1", ".----"),
+                new MorseCodes("2", "..---"),
+                new MorseCodes("3", "...--"),
+                new MorseCodes("4", "....-"),
+                new MorseCodes("5", "....."),
+                new MorseCodes("6", "-...."),
+                new MorseCodes("7", "--..."),
+                new MorseCodes("8", "---.."),
+                new MorseCodes("9", "----."),
+                new MorseCodes("0", "-----"),
+
+                // Umlaute + Diakritisches
+                new MorseCodes("À", ".--.-"),
+                new MorseCodes("Å", ".--.-"),
+                new MorseCodes("Ä", ".-..-"),
+                new MorseCodes("È", ".-..-"),
+                new MorseCodes("É", "..-.."),
+                new MorseCodes("Ö", "---."),
+                new MorseCodes("Ü", "..--"),
+                new MorseCodes("ß", "...--.."),
+                new MorseCodes("CH", "----"), // Muss in späteren Auswertungen besonders berücksichtigt werden!
+                new MorseCodes("Ñ", "--.--"),
+
+                // Zeichen
+                new MorseCodes(".", ".-.-.-"),
+                new MorseCodes(",", "--..--"),
+                new MorseCodes(":", "---..."),
+                new MorseCodes(",", "-.-.-."),
+                new MorseCodes("?", "..--.."),
+                new MorseCodes("!", "-.-.--"),
+                new MorseCodes("-", "-....-"),
+                new MorseCodes("_", "..--.-"),
+                new MorseCodes("(", "-.--."),
+                new MorseCodes(")", "-.--.-"),
+                new MorseCodes("'", ".----."),
+                new MorseCodes("=", "-...-"),
+                new MorseCodes("+", ".-.-."),
+                new MorseCodes("/", "-..-."),
+                new MorseCodes("@", ".--.-."),
+                new MorseCodes("\"", ".-..-."),
+
+                // Signale
+                new MorseCodes("[SOS]", "...---..."),
+                new MorseCodes("[Spruchanfang]", "-.-.-"),
+                new MorseCodes("[Pause]", "-...-"),
+                new MorseCodes("[Spruchende]", ".-.-."),
+                new MorseCodes("[Verstanden]", "...-."),
+                new MorseCodes("[Verkehrsende]", "...-.-"),
+                new MorseCodes("[CQD]", "-.-. --.- -.."),
+                new MorseCodes("[EEEEEEEE]", "..... . . ."),
+
+                // Pause wird ebenfalls als Code verwendet
+                new MorseCodes(" ", "/"),
+                new MorseCodes("\n", "\n"),
+
+
+            };
+
+            Codes = Uebersetzungstabelle;
+
+
+        }
+
+
+
+    }
+
+    public class MorseCodes
     {
 
         private string _letter;
         private string _code;
-        private List<MorseCodes> _knownCodes;
+
         public string Letter
         {
             get { return _letter; }
@@ -20,127 +136,45 @@ namespace MorseCraft
 
         public string Code
         {
-            get {
-                return _code;
-            }
-            set {
-                _code = value;
-            }
+            get { return _code; }
+            set { _code = value; }
         }
 
-        public List<MorseCodes> KnownCodes
-        {
-            get
-            {
-                return _knownCodes;
-            }
-            set
-            {
-                _knownCodes = value;
-            }    
-        }
 
-        private MorseCodes(string letter, string code)
+        public MorseCodes(string letter, string code)
         {
             this.Letter = letter;
-            this.Code = code;  
+            this.Code = code;
         }
 
-        private void InitializeCodes()
+
+        public static string TranslateToText(string code)
+        {
+            return "";
+        }
+
+        public static List<MorseCodes> AnalyseText(string text)
         {
 
-            List<MorseCodes> knownCodes = new List<MorseCodes>();
+            List<MorseCodes> analysierterText = new List<MorseCodes>();
 
-            // Lateinische Buchstaben
-            MorseCodes A = new MorseCodes("A", ".-");
-            MorseCodes B = new MorseCodes("B", "-...");
-            MorseCodes C = new MorseCodes("C", "-.-.");
-            MorseCodes D = new MorseCodes("D", "-..");
-            MorseCodes E = new MorseCodes("E", ".");
-            MorseCodes F = new MorseCodes("F", "..-.");
-            MorseCodes G = new MorseCodes("G", "--.");
-            MorseCodes H = new MorseCodes("H", "....");
-            MorseCodes I = new MorseCodes("I", "..");
-            MorseCodes J = new MorseCodes("J", ".---");
-            MorseCodes K = new MorseCodes("K", "-.-");
-            MorseCodes L = new MorseCodes("L", ".-..");
-            MorseCodes M = new MorseCodes("M", "--");
-            MorseCodes N = new MorseCodes("N", "-.");
-            MorseCodes O = new MorseCodes("O", "---");
-            MorseCodes P = new MorseCodes("P", ".--.");
-            MorseCodes Q = new MorseCodes("Q", "--.-");
-            MorseCodes R = new MorseCodes("R", ".-.");
-            MorseCodes S = new MorseCodes("S", "...");
-            MorseCodes T = new MorseCodes("T", "-");
-            MorseCodes U = new MorseCodes("U", "..-");
-            MorseCodes V = new MorseCodes("V", "...-");
-            MorseCodes W = new MorseCodes("W", ".--");
-            MorseCodes X = new MorseCodes("X", "-..-");
-            MorseCodes Y = new MorseCodes("Y", "-.--");
-            MorseCodes Z = new MorseCodes("Z", "--..");
-            knownCodes.Add(A);
-            knownCodes.Add(B);
-            knownCodes.Add(C);
-            knownCodes.Add(D);
-            knownCodes.Add(E);
+            foreach (char c in text)
+            {
 
-            // Zahlen
-            MorseCodes Eins = new MorseCodes("1", ".----");
-            MorseCodes Zwei = new MorseCodes("2", "..---");
-            MorseCodes Drei = new MorseCodes("3", "...--");
-            MorseCodes Vier = new MorseCodes("4", "....-");
-            MorseCodes Fuenf = new MorseCodes("5", ".....");
-            MorseCodes Sechs = new MorseCodes("6", "-....");
-            MorseCodes Sieben = new MorseCodes("7", "--...");
-            MorseCodes Acht = new MorseCodes("8", "---..");
-            MorseCodes Neun = new MorseCodes("9", "----.");
-            MorseCodes Null = new MorseCodes("0", "-----");
+                string zeichen = c.ToString().ToUpper();
+                // Suche das richtige Element aus der Übersetzungsliste
+                MorseCodes code = CodeList.Codes.FirstOrDefault(item => item.Letter == zeichen);
+                if (code != null)
+                {
+                     analysierterText.Add(code);
+                }
 
-            // Umlaute + Diakritisches
-            MorseCodes A1 = new MorseCodes("À", ".--.-");
-            MorseCodes A2 = new MorseCodes("Å", ".--.-");
-            MorseCodes Ae = new MorseCodes("Ä", ".-..-");
-            MorseCodes E1 = new MorseCodes("È", ".-..-");
-            MorseCodes E2 = new MorseCodes("É", "..-..");
-            MorseCodes Oe = new MorseCodes("Ö", "---.");
-            MorseCodes Ue = new MorseCodes("Ü", "..--");
-            MorseCodes Sz = new MorseCodes("ß", "...--..");
-            MorseCodes Ch = new MorseCodes("CH", "----"); // Muss in späteren Auswertungen besonders berücksichtigt werden!
-            MorseCodes N1 = new MorseCodes("Ñ", "--.--");
+            }
 
-
-            // Zeichen
-            MorseCodes Punkt = new MorseCodes(".", ".-.-.-"); 
-            MorseCodes Komma = new MorseCodes(",", "--..--"); 
-            MorseCodes Doppelpunkt = new MorseCodes(":", "---...");
-            MorseCodes Semikolon = new MorseCodes(";", "-.-.-.");
-            MorseCodes Fragezeichen = new MorseCodes("?", "..--..");
-            MorseCodes Ausrufezeichen = new MorseCodes("!", "-.-.--");
-            MorseCodes Bindestrich = new MorseCodes("-", "-....-");
-            MorseCodes Unterstrich = new MorseCodes("_", "..--.-");
-            MorseCodes KlammerAuf = new MorseCodes("(", "-.--.");
-            MorseCodes KlammerZu = new MorseCodes(")", "-.--.-");
-            MorseCodes Hochkomma = new MorseCodes("'", ".----.");
-            MorseCodes IstGleich = new MorseCodes("=", "-...-");
-            MorseCodes Plus = new MorseCodes("+", ".-.-.");
-            MorseCodes GeteiltDurch = new MorseCodes("/", "-..-.");
-            MorseCodes AT = new MorseCodes("@", ".--.-.");
-            MorseCodes KlaAnfuehrungszeichen = new MorseCodes("\"", ".-..-.");
-
-            // Signale
-            MorseCodes SOS = new MorseCodes("[SOS]", "...---...");
-            MorseCodes Spruchanfang = new MorseCodes("[Spruchanfang]", "-.-.-");
-            MorseCodes Pause = new MorseCodes("[Pause]", "-...-");
-            MorseCodes Spruchende = new MorseCodes("[Spruchende]", ".-.-.");
-            MorseCodes Verstanden = new MorseCodes("[Verstanden]", "...-.");
-            MorseCodes Verkehrsende = new MorseCodes("[Verkehrsende]", "...-.-");
-            MorseCodes FtNotruf = new MorseCodes("[CQD]", "-.-. --.- -..");
-            MorseCodes FehlerIrrung = new MorseCodes("[EEEEEEEE]", "..... . . .");
-
-
-
-
+            ;
+            return analysierterText;
         }
 
     }
 }
+
--- MorseCraft/Program.cs
+++ MorseCraft/Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace MorseCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CodeList.Initialize();
+
+            if (args[0] == "-t")
+            {
+
+                List<MorseCodes> codes = MorseCodes.AnalyseText(args[1]);
+                foreach (MorseCodes code in codes)
+                {
+                    Console.Write(code.Code);
+                    Console.Write(" ");
+
+                }
+            }
+
+
+            if (args[0] == "-b")
+            {
+
+                List<MorseCodes> codes = MorseCodes.AnalyseText(args[1]);
+
+                ExecuteMorseCode(codes,true,true,false,350);
+
+            }
+
+
+        }
+
+        public static void ExecuteMorseCode(List<MorseCodes> codeList,bool withTextoutput, bool withSound, bool withGraphics, int dithLength)
+        {
+
+            int ditLength = dithLength;
+            int dahLength = 3 * dithLength;
+            int dohLength = 7* dithLength;
+            int pauseBetweenChars = dithLength;
+            int ditCounter = 0;
+
+            foreach (MorseCodes code in codeList)
+            {
+                foreach (char c in code.Code)
+                {
+
+                    if (withTextoutput == true)
+                    {
+                        Console.Write(c);
+
+                    }
+
+
+                    switch (c.ToString())
+                    {
+                        case ".":
+                            if (withSound == true)
+                            {
+                                Console.Beep(1000,dithLength);
+                                ditCounter++;
+                            }
+                            Thread.Sleep(pauseBetweenChars);
+                            break;
+                        case "-":
+                            if (withSound == true)
+                            {
+                                Console.Beep(1000, dahLength);
+                                ditCounter++;
+                            }
+                            Thread.Sleep(pauseBetweenChars);
+                            break;
+                        case "/":
+                            Thread.Sleep(dohLength);
+                            break;
+                        default:
+                            break;
+
+                    }
+
+      
+                }
+
+                if (ditCounter == 2)
+                {
+                    Thread.Sleep(ditLength);
+                    ditCounter = 0;
+                }
+
+                if (withTextoutput == true)
+                {
+                    Console.Write(" ");
+                }
+
+            }
+        }
+    }
+}
